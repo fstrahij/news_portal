5975487cffdf011f106d5255be9676fc7d1954d5
fstrahij <filip2893@gmail.com>
2019-03-17 15:51:00 +0100

    models created

--- news_portal/news/models.py
+++ news_portal/news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
 # Create your models here.
+class Channel(models.Model):
+	title = models.CharField(max_length = 30, primary_key = True)
+	link = models.URLField()
+	description = models.TextField(null = True)
+	language = models.CharField(max_length = 10)
+	lastBuildDate = models.DateTimeField()
+
+class Item(models.Model):
+	title = models.CharField(max_length = 30, primary_key = True)
+	link = models.URLField()
+	description = models.TextField()
+	image = models.ImageField(blank = True, null = True)
+	language = models.CharField(max_length = 10)
+	pubDate = models.DateTimeField()
+	guid = models.URLField()
+	category = models.ForeignField(Channel)
