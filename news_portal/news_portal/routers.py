401188f881bc7c92d68d1b1241e73cd71f1c314b
fstrahij <filip2893@gmail.com>
2019-03-18 10:31:19 +0100

    routers created

---
+++ news_portal/news_portal/routers.py
@@ -0,0 +1,7 @@
+from rest_framework import routers
+from news.viewsets import ChannelViewSet, ItemViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'channel', ChannelViewSet)
+router.register(r'item', ItemViewSet)
