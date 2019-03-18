7a9f3e046f6370adead2c84fe5c75052f3ce8a0c
fstrahij <filip2893@gmail.com>
2019-03-18 10:51:10 +0100

    polls dir removed

--- news_portal/polls/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-	hello = '<h1>hello word</h1>'
-	return HttpResponse(hello)
-# Create your views here.
