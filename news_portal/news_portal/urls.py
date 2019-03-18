7b9d9fc4dcee6d141c5d9f98d9ad296a76d5d552
fstrahij <filip2893@gmail.com>
2019-03-18 10:33:59 +0100

    router path added

--- news_portal/news_portal/urls.py
+++ news_portal/news_portal/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from .routers import router
 
 urlpatterns = [
 	path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
